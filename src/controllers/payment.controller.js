f58740569e84ad96ea22e1181c37158c5f3c9dd9
Dixit soni <dixitsoni424@gmail.com>
2025-12-28 01:13:22 +0530

    changes in code

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -28,7 +28,6 @@ export const paymentStripe = async (req, res) => {
     const { amount, invoiceId } = req.body;
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-      payment_method_types: ["cashapp"],
       line_items: [
         {
           price_data: {
